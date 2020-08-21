eee8ee7f35698ffc0d391f1b440a0d957b53afb5
n3xem <yukyanjpn@gmail.com>
2020-08-21 23:03:27 +0900

    Add: 鍵盤を押したときのアニメーション

--- src/app.ts
+++ src/app.ts
@@ -43,9 +43,11 @@ class ThreeJSContainer {
     private createScene = () => {
         const synth = new TONE.Synth().toDestination();
         const WHITE_KEY_WIDTH = 2;
+        const WHITE_KEY_DEPTH = 10;
+        const BLACK_KEY_DEPTH = 6;
         const WHITE_KEY_NUM = 7;
         const WHITE_KEY_DISTANCE = 0.1;
-        const KEYBOARD_WIDTH = 2.1 * 6;
+        const KEYBOARD_WIDTH = WHITE_KEY_WIDTH * WHITE_KEY_NUM + 0.1 * (WHITE_KEY_NUM - 1);
         const WHITE_KEY_SCALE: string[] = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
         const BLACK_KEY_SCALE: string[] = ["C#4", "D#4", "", "F#4", "G#4", "A#4"];
 
@@ -54,38 +56,49 @@ class ThreeJSContainer {
 
         this.scene = new THREE.Scene();
 
-        let white_key_geometry = new THREE.BoxGeometry(WHITE_KEY_WIDTH, 2, 10);
+        let white_key_geometry = new THREE.BoxGeometry(WHITE_KEY_WIDTH, 2, WHITE_KEY_DEPTH);
         white_key_geometry.name = "white";
+        white_key_geometry.translate(0, 0, WHITE_KEY_DEPTH / 2);
         let black_key_geometry = new THREE.BoxGeometry(WHITE_KEY_WIDTH / 2, 2, 6);
         black_key_geometry.name = "black";
+        black_key_geometry.translate(0, 0, BLACK_KEY_DEPTH / 2);
 
         let white_keys: THREE.Mesh[] = new Array(7);
         let black_keys: THREE.Mesh[] = new Array(5);
 
         let meshList: THREE.Mesh[] = [];
 
-        //白い鍵盤を追加
-        for (let i = 0; i < WHITE_KEY_NUM; i++) {
-            let white_key_material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF });
-            let tmp: THREE.Mesh = new THREE.Mesh(white_key_geometry, white_key_material);
-            tmp.position.set(i * (WHITE_KEY_WIDTH + WHITE_KEY_DISTANCE) - KEYBOARD_WIDTH / 2, 0, 0);
-            tmp.name = WHITE_KEY_SCALE[i];
-            white_keys[i] = tmp;
-            meshList.push(tmp);
-            this.scene.add(white_keys[i]);
+        let createKeyboard = (octave: number, x: number) => {
+            let WHITE_KEY_SCALE: string[] = ["C", "D", "E", "F", "G", "A", "B"];
+            let BLACK_KEY_SCALE: string[] = ["C#", "D#", "", "F#", "G#", "A#"];
+
+            //白い鍵盤を追加
+            for (let i = 0; i < WHITE_KEY_NUM; i++) {
+                let white_key_material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF });
+                let tmp: THREE.Mesh = new THREE.Mesh(white_key_geometry, white_key_material);
+                tmp.position.set(i * (WHITE_KEY_WIDTH + WHITE_KEY_DISTANCE) + x, 0, - WHITE_KEY_DEPTH / 2);
+                tmp.name = WHITE_KEY_SCALE[i] + octave;
+                white_keys[i] = tmp;
+                meshList.push(tmp);
+                this.scene.add(white_keys[i]);
+            }
+
+            //黒い鍵盤を追加
+            for (let i = 0; i < 6; i++) {
+                if (i === 2) continue;
+                let black_key_material = new THREE.MeshPhongMaterial({ color: 0x000000 });
+                let tmp: THREE.Mesh = new THREE.Mesh(black_key_geometry, black_key_material);
+                tmp.position.set(1.05 + i * (WHITE_KEY_WIDTH + WHITE_KEY_DISTANCE) + x, 1, -1.7 - BLACK_KEY_DEPTH / 2);
+                tmp.name = BLACK_KEY_SCALE[i] + octave;
+                black_keys[i] = tmp;
+                meshList.push(tmp);
+                this.scene.add(black_keys[i]);
+            }
+
         }
 
-        //黒い鍵盤を追加
-        for (let i = 0; i < 6; i++) {
-            if (i === 2) continue;
-            let black_key_material = new THREE.MeshPhongMaterial({ color: 0x000000 });
-            let tmp: THREE.Mesh = new THREE.Mesh(black_key_geometry, black_key_material);
-            tmp.position.set(1.05 + i * (WHITE_KEY_WIDTH + WHITE_KEY_DISTANCE) - KEYBOARD_WIDTH / 2, 1, -1.7);
-            tmp.name = BLACK_KEY_SCALE[i];
-            black_keys[i] = tmp;
-            meshList.push(tmp);
-            this.scene.add(black_keys[i]);
-        }
+        createKeyboard(4, -KEYBOARD_WIDTH - 0.1);
+        createKeyboard(5, 0);
 
         //ライトの設定
         const spotLight = new THREE.SpotLight(0xFFFFFF, 2, 50, Math.PI / 4, 10, 0.5);
@@ -148,17 +161,20 @@ class ThreeJSContainer {
                     console.log("attack");
                     if (playedScale[mesh.name] !== true) {
                         synth.triggerAttack(mesh.name, now);
+                        mesh.rotation.x = 0.1;
                         playedScale[mesh.name] = true;
                     }
-                    mesh.material.color.setHex(0xFF0000);
+                    //mesh.material.color.setHex(0xFF0000);
                 }
                 else if (mesh.geometry.name === "black") {
                     playedScale[mesh.name] = false;
-                    mesh.material.color.setHex(0x000000)
+                    mesh.material.color.setHex(0x000000);
+                    mesh.rotation.x = 0;
                 }
                 else if (mesh.geometry.name === "white") {
                     playedScale[mesh.name] = false;
                     mesh.material.color.setHex(0xFFFFFF);
+                    mesh.rotation.x = 0;
                 }
             });
 
@@ -170,5 +186,5 @@ class ThreeJSContainer {
 
 let container = new ThreeJSContainer();
 
-let viewport = container.createRendererDOM(1200, 500, new THREE.Vector3(0, 10, 10));
+let viewport = container.createRendererDOM(1200, 500, new THREE.Vector3(0, 13, 10));
 document.body.appendChild(viewport);
